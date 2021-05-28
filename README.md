27b57c030b3d139ae5812a1f1cbfb5c88bcacdb5
Johannes <johannes-pahle@t-online.de>
2021-05-28 23:00:23 +0200

    title; canvas;

---
+++ README.md
@@ -0,0 +1,1 @@
+# zustandsübergangsdiagramm
---
+++ script.js
@@ -0,0 +1,8 @@
+var c = document.getElementById("canvas");
+canvas.width = window.innerWidth - 10;
+canvas.height = window.innerHeight - 90;
+
+var zustaende = [{ x: 0, y: 0 }];
+
+var ctx = c.getContext("2d");
+
