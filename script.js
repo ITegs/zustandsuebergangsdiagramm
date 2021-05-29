9b3033005b40ed7be170f2b71dfa2ec47b0f9392
Johannes <johannes-pahle@t-online.de>
2021-05-29 23:42:49 +0200

    Arrows;

--- script.js
+++ script.js
@@ -7,34 +7,49 @@ var zustaende = [
   { name: "lol", x: 200, y: 200, connect: [1] },
   { name: "yeet", x: 200, y: 100, connect: [2] },
   { name: "yolo", x: 750, y: 20, connect: [3] },
-  { name: "lmao", x: 450, y: 200, connect: [0] },
+  { name: "lmao", x: 450, y: 200, connect: [0, 2] },
 ];
 
 draw();
 
 function draw() {
   connections();
-  labels();
+    labels();
 }
 
 function connections() {
+  //   zustaende.map((zustand) => {
+  //     ctx.moveTo(zustand.x, zustand.y);
+  //     zustand.connect.map((connect) => {
+  //       ctx.lineTo(zustaende[connect].x, zustaende[connect].y);
+  //     });
+  //     ctx.stroke();
+  //   });
+
   zustaende.map((zustand) => {
-    ctx.moveTo(zustand.x, zustand.y);
+    ctx.fillStyle = "#489FB5"
     zustand.connect.map((connect) => {
-      ctx.lineTo(zustaende[connect].x, zustaende[connect].y);
+      ctx.beginPath();
+      ctx.arrow(
+        zustand.x,
+        zustand.y,
+        zustaende[connect].x,
+        zustaende[connect].y,
+        [10, 10, -10, 1, -10, 15]
+      );
+      ctx.fill();
     });
-    ctx.stroke();
   });
 }
 
 function labels() {
-  ctx.font = "25px sans-serif";
+  ctx.font = "25px Lexend Deca";
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
   zustaende.map((zustand) => {
-    ctx.fillStyle = "#fff";
+    ctx.fillStyle = "#AEC3B0";
     ctx.fillRect(zustand.x - 30, zustand.y - 30, 60, 60);
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = "#01161E";
     ctx.fillText(zustand.name, zustand.x, zustand.y);
   });
 }
