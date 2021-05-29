1e7e75a5415289e51e1a9364b47126384116b10d
Johannes <johannes-pahle@t-online.de>
2021-05-29 23:02:58 +0200

    connections; names;

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # zustandsübergangsdiagramm
+
+Automatically created and optimized statecharts.
+
+### `zustaende`-Syntax:
+
+[\
+&emsp;{\
+&emsp;&emsp;name: string,\
+&emsp;&emsp;x: number,\
+&emsp;&emsp;y: number,\
+&emsp;&emsp;connect: ['position in `zusteande`'],\
+&emsp;},\
+&emsp;...\
+]
--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 var c = document.getElementById("canvas");
 canvas.width = window.innerWidth - 10;
 canvas.height = window.innerHeight - 90;
-
-var zustaende = [{ x: 0, y: 0 }];
-
 var ctx = c.getContext("2d");
 
+var zustaende = [
+  { name: "lol", x: 200, y: 200, connect: [1] },
+  { name: "yeet", x: 200, y: 100, connect: [2] },
+  { name: "yolo", x: 750, y: 20, connect: [3] },
+  { name: "lmao", x: 450, y: 200, connect: [0] },
+];
+
+draw();
+
+function draw() {
+  connections();
+  labels();
+}
+
+function connections() {
+  zustaende.map((zustand) => {
+    ctx.moveTo(zustand.x, zustand.y);
+    zustand.connect.map((connect) => {
+      ctx.lineTo(zustaende[connect].x, zustaende[connect].y);
+    });
+    ctx.stroke();
+  });
+}
+
+function labels() {
+  ctx.font = "25px sans-serif";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  zustaende.map((zustand) => {
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(zustand.x - 30, zustand.y - 30, 60, 60);
+    ctx.fillStyle = "#000";
+    ctx.fillText(zustand.name, zustand.x, zustand.y);
+  });
+}
