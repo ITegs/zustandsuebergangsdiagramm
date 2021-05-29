e31707464294785e42da0236e9cdc3dc4f949a24
Johannes <johannes-pahle@t-online.de>
2021-05-30 01:00:43 +0200

    add-button;

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var c = document.getElementById("canvas");
 canvas.width = window.innerWidth - 10;
-canvas.height = window.innerHeight - 90;
+canvas.height = window.innerHeight - 110;
 var ctx = c.getContext("2d");
 
 var zustaende = [
   { name: "lol", x: 200, y: 200, connect: [1] },
   { name: "yeet", x: 200, y: 100, connect: [2, 3] },
-  { name: "yolo", x: 750, y: 20, connect: [3] },
+  { name: "yolo", x: 750, y: 50, connect: [3] },
   { name: "lmao", x: 450, y: 200, connect: [0, 2] },
 ];
 
@@ -40,7 +40,7 @@ function labels() {
   ctx.textBaseline = "middle";
   zustaende.map((zustand) => {
     ctx.fillStyle = "#C0C0C0";
-    ctx.fillRect(zustand.x - zustand.name.length*20/2, zustand.y - 23, zustand.name.length*20, 46);
+    ctx.fillRect(zustand.x - zustand.name.length*17/2, zustand.y - 23, zustand.name.length*17, 46);
     ctx.fillStyle = "#004E98";
     ctx.fillText(zustand.name, zustand.x, zustand.y);
   });
